76d6e6584d68b567b0e9d3cb17c3282d1ea5c6b4
Евгений Свиридов <svirid.evg@yandex.ru>
2024-12-03 01:44:22 +0300

    debug

--- src/app/signup/successfully/page.tsx
+++ src/app/signup/successfully/page.tsx
@@ -16,14 +16,16 @@ export default function Successfully() {
       <div className={styles.containerSignup}>
         <div className={styles.modalBlock}>
           <div className={styles.modalFormLogin}>
-            <div className={styles.modalLogo}>
-              <Image
-                src="/img/logo_modal.png"
-                alt="logo"
-                width={140}
-                height={21}
-              />
-            </div>
+            <a>
+              <div className={styles.modalLogo}>
+                <Image
+                  src="/img/logo_modal.png"
+                  alt="logo"
+                  width={140}
+                  height={21}
+                />
+              </div>
+            </a>
             <div>
               <p className={styles.successText}>
                 Вы успешно зарегистрированы. Пожалуйста, перейдите на страницу
@@ -31,7 +33,7 @@ export default function Successfully() {
               </p>
             </div>
             <button onClick={handleClick} className={styles.modalBtnSignupEnt}>
-              Авторизоваться
+              <a>Авторизоваться</a>
             </button>
           </div>
         </div>
